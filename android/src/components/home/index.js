ba5b3f8f08a566750cd8bcb39e0ff2c8da8219af
Andy Li <andyzanli@gmail.com>
2017-05-24 23:41:44 -0700

    Revert "Revert "initialized sorting for homepage listview""
    This reverts commit dde601ee603e0f1c5b6579fb2f77765d205cb9c9.

--- android/src/components/home/index.js
+++ android/src/components/home/index.js
@@ -34,7 +34,7 @@ export default class Home extends Component {
         <TicketCounter />
         <TimersList navigation={this.props.navigation} />
       </View>
-    )
+    );
   }
 
   // renderTimerLists() {
