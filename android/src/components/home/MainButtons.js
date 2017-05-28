117a29ffcc784ed8f69e61f99d60f7bcfb864f1b
Andy Li <andyzanli@gmail.com>
2017-05-28 10:49:30 -0700

    changed underlay to show and hide a border bottom

--- android/src/components/home/MainButtons.js
+++ android/src/components/home/MainButtons.js
@@ -8,23 +8,45 @@ import {
 } from 'react-native';
 
 
-const MainButtons = (props) => (
-  <View style={styles.mainButtonsContainer}>
-    <TouchableHighlight
-      onPress={() => props.navigation.navigate('Map')}
-      underlayColor="#4286f4"
-      style={styles.mapButton} >
-      <Image source={require('../../../../shared/images/white-pin.png')} />
-    </TouchableHighlight>
-    <View style={styles.separator} />
-    <TouchableHighlight
-      underlayColor="#4286f4"
-      onPress={() => props.navigation.navigate('Camera')}
-      style={styles.cameraButton} >
-      <Image source={require('../../../../shared/images/camera.png')} />
-    </TouchableHighlight>
-  </View>
-);
+export default class MainButtons extends Component {
+  constructor() {
+    super();
+    this.state = {
+      map: false,
+      camera: false,
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.mainButtonsContainer}>
+        <TouchableHighlight
+          style={ this.state.map ? styles.mapPressed : styles.button }
+          onHideUnderlay={() => {this._onHideUnderlay('map')}}
+          onShowUnderlay={() => {this._onShowUnderlay('map')}}
+          onPress={() => this.props.navigation.navigate('Map')} >
+          <Image source={require('../../../../shared/images/white-pin.png')} />
+        </TouchableHighlight>
+        <View style={styles.separator} />
+        <TouchableHighlight
+          style={ this.state.camera ? styles.cameraPressed : styles.button }
+          onHideUnderlay={() => {this._onHideUnderlay('camera')}}
+          onShowUnderlay={() => {this._onShowUnderlay('camera')}}
+          onPress={() => this.props.navigation.navigate('Camera')} >
+          <Image source={require('../../../../shared/images/camera.png')} />
+        </TouchableHighlight>
+      </View>
+    );
+  }
+
+  _onShowUnderlay(button) {
+    button === 'map' ? this.setState({map: true}) : this.setState({camera: true});
+  }
+
+  _onHideUnderlay(button) {
+    button === 'map' ? this.setState({map: false}) : this.setState({camera: false});
+  }
+}
 
 const styles = StyleSheet.create({
   mainButtonsContainer: {
@@ -32,11 +54,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#4286f4',
   },
-  mapButton: {
+  button: {
     flex: .5,
     height: 70,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mapPressed: {
+    flex: .5,
+    height: 70,
+    backgroundColor: '#4286f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 5,
+    borderColor: 'white',
+  },
+  cameraPressed: {
+    flex: .5,
+    height: 70,
+    backgroundColor: '#4286f4',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 5,
+    borderColor: 'white',
   },
   mapButtonText: {
     color: 'white',
@@ -47,16 +87,8 @@ const styles = StyleSheet.create({
     borderWidth: .5,
     height: 40,
   },
-  cameraButton: {
-    flex: .5,
-    height: 70,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   cameraButtonText: {
     color: 'white',
     fontSize: 18,
   }
 });
-
-export default MainButtons;
