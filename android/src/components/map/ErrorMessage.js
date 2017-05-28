d917b66963e5d0d14ccfdb4ac3dda8c815bc12be
Andy Li <andyzanli@gmail.com>
2017-05-28 15:48:12 -0700

    included error message for no location service

---
+++ android/src/components/map/ErrorMessage.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+const ErrorMessage = () => (
+  <View style={styles.container}>
+    <Text style={styles.message}> Location not accessible. Please try again later. </Text>
+  </View>
+);
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  // container: {
+  //   position: 'absolute',
+  //   bottom: 0,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   backgroundColor: 'white',
+  //   zIndex: 10,
+  // },
+  message: {
+    padding: 18,
+    fontSize: 16,
+  }
+});
+
+styles.container = {
+  position: 'absolute',
+  bottom: 0,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'white',
+  width: width,
+  zIndex: 10,
+  borderTopWidth: 2,
+  borderColor: '#4286f4',
+}
+
+export default ErrorMessage;
