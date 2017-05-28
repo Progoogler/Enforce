76ca6bce93412118efe1bc3d41ce51888f2b1d8a
Andy Li <andyzanli@gmail.com>
2017-05-28 15:49:29 -0700

    added location services diagloue box library

--- android/app/src/main/java/com/quicket/MainApplication.java
+++ android/app/src/main/java/com/quicket/MainApplication.java
@@ -3,6 +3,7 @@ package com.quicket;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.rnfs.RNFSPackage;
 import io.realm.react.RealmReactPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LocationServicesDialogBoxPackage(),
             new RNFSPackage(),
             new RealmReactPackage(),
             new RCTCameraPackage(),
