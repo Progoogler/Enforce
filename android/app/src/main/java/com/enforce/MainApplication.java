3f72b2881d67aae80dfa03826f413086ff404fb8
Andy Li <andyzanli@gmail.com>
2017-06-03 16:03:54 -0700

    linked RNFetchBlob

--- android/app/src/main/java/com/enforce/MainApplication.java
+++ android/app/src/main/java/com/enforce/MainApplication.java
@@ -3,6 +3,7 @@ package com.enforce;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.rnfs.RNFSPackage;
 import io.realm.react.RealmReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFetchBlobPackage(),
             new LocationServicesDialogBoxPackage(),
             new RNFSPackage(),
             new RealmReactPackage(),
