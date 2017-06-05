8ffe96171d916f79f1fc5554c55c30efafd8f99f
Andy Li <andyzanli@gmail.com>
2017-06-04 20:34:37 -0700

    passed prop to ticketCount w/ state update

--- android/src/components/home/index.js
+++ android/src/components/home/index.js
@@ -16,6 +16,9 @@ import TimersList from './ListView';
 export default class Home extends Component {
   constructor() {
     super();
+    this.state = {
+      ticketCount: 0,
+    }
     this.realm = new Realm();
   }
 
@@ -34,11 +37,20 @@ export default class Home extends Component {
       <View style={styles.container} >
         <Header navigation={this.props.navigation} />
         <MainButtons navigation={this.props.navigation} />
-        <TicketCounter realm={this.realm} />
-        <TimersList navigation={this.props.navigation} />
+        <TicketCounter realm={this.realm} ticketCount={this.state.ticketCount} />
+        <TimersList navigation={this.props.navigation} updateTicketCount={this.updateTicketCount.bind(this)}/>
       </View>
     );
   }
+
+  componentWillMount() {
+    this.ticketCount = this.realm.objects('Ticketed')[0] ? this.realm.objects('Ticketed')[0].list.length : 0;
+  }
+
+  updateTicketCount() {
+    this.setState({ticketCount: 0});
+  }
+
 }
 
 const styles = StyleSheet.create({
