978154fe379dfd202c5ada618c904b6c27977039
Andy Li <andyzanli@gmail.com>
2017-06-04 20:37:31 -0700

    moved app reset functionality to ListView of Overview

--- android/src/components/App.js
+++ android/src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { DrawerNavigator } from 'react-navigation';
 import { AsyncStorage } from 'react-native';
-import RNFS from 'react-native-fs';
 import { CameraApp } from './camera/CameraApp';
 import MapApp from './map/MapApp';
 import Home from './home';
@@ -72,51 +71,10 @@ export default class App extends Component {
     // }); // For beta testing only TODO remove this
 
 
-    let timerLists = this.realm.objects('Timers');
-    let ticketList = this.realm.objects('Ticketed');
-    let expiredList = this.realm.objects('Expired');
-    if (timerLists.length >= 1) { // Initializing Timers automatically gives it a length of 1 with an empty list object
-      let i = 0, lastTime;
-      while (lastTime === undefined && (timerLists[i + 1] || ticketList[0].list[i + 1] || expiredList[0].list[i + 1])) { // Edge case for empty first object
-        if (timerLists[i].list.length > 0) {
-          lastTime = timerLists[i].list[0].createdAt;
-          break;
-        }
-        if (ticketList[0].list.length > 0) {
-          lastTime = ticketList[0].list[i].createdAt;
-          break;
-        }
-        if (expiredList[0].list.length > 0) {
-          lastTime = expiredList[0].list[i].createdAt;
-          break;
-        }
-        i++;
-      }
-      let context = this;
-      let now = new Date();
-      if (!lastTime || now - lastTime > 28800000) { // Reset DB after 8 hours of activity
-        this._loopDeletion(timerLists);
-        this._loopDeletion(ticketList, true);
-        this._loopDeletion(expiredList, true);
-        console.log('deletion')
-        //TODO Doesn't wait..
-        setTimeout(() => {
-          console.log('DELETE')
-          Realm.clearTestState();
-          this.realm = new Realm({schema: Schema});
-          this.realm.write(() => {
-            this.realm.create('TimerSequence', {timeAccessedAt: new Date() / 1, count: 0});
-            this.realm.create('TimeLimit', {float: 1, hour: '1', minutes: "00"});
-            this.realm.create('Coordinates', {latitude: 0, longitude: 0});
-            this.realm.create('Ticketed', {list: []});
-            this.realm.create('Expired', {list: []});
-          });
-        }, 1000);
-      }
-    }
 
+    //console.log('reset?', this.props.navigation.state.params.reset)
     return (
-        <AppNavigator/>
+        <AppNavigator />
     );
   }
   async signIn() {
@@ -126,38 +84,5 @@ export default class App extends Component {
     console.log('signed in user!');
   };
 
-  _loopDeletion(timerLists, once) {
-    if (once) {
-      timerLists[0].list.forEach((timer, idx) => {
-        RNFS.unlink(timer.mediaPath)
-        .then(() => {
-          console.log('FILE DELETED');
-          RNFS.exists(timer.mediaUri)
-          .then(() => {
-            console.log('PICTURE REMOVED');
-            this.realm.write(() => {
-              this.realm.delete(timerLists[0]['list'][idx]);
-            });
-          });
-        });
-      });
-    } else {
-      timerLists.forEach((timerList, idx) => {
-        timerList.list.forEach((timer, sidx) => {
-          RNFS.unlink(timer.mediaPath)
-          .then(() => {
-            console.log('FILE DELETED');
-            RNFS.exists(timer.mediaUri)
-            .then(() => {
-              console.log('PICTURE REMOVED');
-              this.realm.write(() => {
-                //timerLists[idx]['list'].pop();
-                this.realm.delete(timerLists[idx]['list'][sidx]);
-              });
-            });
-          });
-        });
-      });
-    }
-  }
+
 }
