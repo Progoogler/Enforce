ef3cd6485f7a983e629e8e986bff6e3ccf03ad20
Andy Li <andyzanli@gmail.com>
2017-06-06 23:10:28 -0700

    fade in and out New Timer notification

--- android/src/components/camera/Notification.js
+++ android/src/components/camera/Notification.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View,
+  Animated,
+  Easing,
   Text,
   TouchableHighlight,
 } from 'react-native';
 
-const Notification = () => (
-  <View style={styles.container}>
-    <Text style={styles.text}>New Timer</Text>
-  </View>
-);
+export default class Notification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+      color: new Animated.Value(0),
+    };
+  }
+
+  render() {
+    return (
+      <Animated.View style={
+        { ...this.props.style,
+          opacity: this.state.fadeAnim, } } >
+        <Text style={styles.text} >New Timer</Text>
+      </Animated.View>
+    );
+  }
+
+  componentDidMount() {
+    Animated.timing(  // Animate over time
+      this.state.fadeAnim,  // The animated value to drive
+      { toValue: 1 },  // Animate to opacity: 1, or fully opaque
+    ).start();
+  // this.state.color.interpolate({
+  //   inputRange: [0, 140],
+  //   outputRange: ['rgba(0, 0, 0, 1)', 'rgba(140, 140, 140, 1)'],
+  // });
+    setTimeout(() => {
+      Animated.timing(
+        this.state.fadeAnim,
+        { toValue: 0,
+          duration: 500, },
+      ).start();
+    }, 1000);
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -18,10 +51,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   text: {
-    color: 'white',
+    color: 'rgba(255, 255, 255, 1.0)',
     fontWeight: 'bold',
     fontSize: 28,
   },
 });
-
-export default Notification;
