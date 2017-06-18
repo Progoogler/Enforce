b18959227663705436dee270eb6c4998f8001367
Andy Li <andyzanli@gmail.com>
2017-06-18 13:03:13 -0700

    separated the underline element into a separate style for potentially faster render

--- android/src/components/overview/MainButtons.js
+++ android/src/components/overview/MainButtons.js
@@ -14,37 +14,51 @@ export default class MainButtons extends Component {
     this.state = {
       map: false,
       camera: false,
+      mapUnderline: false,
+      cameraUnderline: false,
     }
   }
 
   render() {
     return (
       <View style={styles.mainButtonsContainer}>
-        <TouchableHighlight
-          style={ this.state.map ? styles.mapPressed : styles.button }
-          onHideUnderlay={() => {this._onHideUnderlay('map')}}
-          onShowUnderlay={() => {this._onShowUnderlay('map')}}
-          onPress={() => this.props.navigation.navigate('Map')} >
-          <Image source={require('../../../../shared/images/white-pin.jpg')} />
-        </TouchableHighlight>
+
+        <View style={styles.buttonColumn}>
+          <TouchableHighlight
+            style={styles.button}
+            underlayColor='#4286f4'
+            onHideUnderlay={() => {this._onHideUnderlay('map')}}
+            onShowUnderlay={() => {this._onShowUnderlay('map')}}
+            onPress={() => this.props.navigation.navigate('Map')} >
+            <Image source={require('../../../../shared/images/white-pin.jpg')} />
+          </TouchableHighlight>
+          { this.state.mapUnderline ? <View style={styles.underline} /> : null }
+        </View>
+
         <View style={styles.separator} />
-        <TouchableHighlight
-          style={ this.state.camera ? styles.cameraPressed : styles.button }
-          onHideUnderlay={() => {this._onHideUnderlay('camera')}}
-          onShowUnderlay={() => {this._onShowUnderlay('camera')}}
-          onPress={() => this.props.navigation.navigate('Camera')} >
-          <Image source={require('../../../../shared/images/camera.png')} />
-        </TouchableHighlight>
+
+        <View style={styles.buttonColumn}>
+          <TouchableHighlight
+            style={ styles.button }
+            underlayColor='#4286f4'
+            onHideUnderlay={() => {this._onHideUnderlay('camera')}}
+            onShowUnderlay={() => {this._onShowUnderlay('camera')}}
+            onPress={() => this.props.navigation.navigate('Camera')} >
+            <Image source={require('../../../../shared/images/camera.png')} />
+          </TouchableHighlight>
+          { this.state.cameraUnderline ? <View style={styles.underline} /> : null }
+        </View>
+
       </View>
     );
   }
 
   _onShowUnderlay(button) {
-    button === 'map' ? this.setState({map: true}) : this.setState({camera: true});
+    button === 'map' ? this.setState({mapUnderline: true}) : this.setState({cameraUnderline: true});
   }
 
   _onHideUnderlay(button) {
-    button === 'map' ? this.setState({map: false}) : this.setState({camera: false});
+    button === 'map' ? this.setState({mapUnderline: false}) : this.setState({cameraUnderline: false});
   }
 }
 
@@ -54,8 +68,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#4286f4',
   },
-  button: {
+  buttonColumn: {
     flex: .5,
+    flexDirection: 'column',
+  },
+  underline: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    borderWidth: 5,
+    borderColor: 'white',
+  },
+  button: {
     height: 70,
     paddingBottom: 10,
     alignItems: 'center',
@@ -65,26 +89,6 @@ const styles = StyleSheet.create({
     borderColor: 'white',
     borderWidth: .35,
     height: 35,
-  },
-  mapPressed: {
-    flex: .5,
-    height: 60,
-    paddingBottom: 10,
-    backgroundColor: '#4286f4',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 5,
-    borderColor: 'white',
-  },
-  cameraPressed: {
-    flex: .5,
-    height: 60,
-    paddingBottom: 10,
-    backgroundColor: '#4286f4',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderBottomWidth: 5,
-    borderColor: 'white',
   },
   mapButtonText: {
     color: 'white',
