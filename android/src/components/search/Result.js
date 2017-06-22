7817b4accdd1e403dc755c67d60e1d0f1d3e4eec
Andy Li <andyzanli@gmail.com>
2017-06-22 00:17:10 -0700

    fixed multiple proptypes for data

--- android/src/components/search/Result.js
+++ android/src/components/search/Result.js
@@ -123,7 +123,7 @@ export default class Result extends Component {
 }
 
 Result.propTypes = {
-  data: PropTypes.object || PropTypes.string,
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   license: PropTypes.string,
   minimizeResultContainer: PropTypes.func.isRequired,
   resizeMenuContainer: PropTypes.func,
