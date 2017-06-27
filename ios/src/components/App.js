574faaa75393a2b102729014c8a9e3a627ed9cf2
Ray <Ray@rays-air.attlocal.net>
2017-06-26 20:30:10 -0700

    commented out sections of code

--- ios/src/components/App.js
+++ ios/src/components/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+  import React, { Component } from 'react';
 import { DrawerItems, DrawerNavigator } from 'react-navigation';
 import { View, Image, AsyncStorage } from 'react-native';
 
-// import CameraApp from './camera/CameraApp';
+ import CameraApp from './camera/CameraApp';
 // import MapApp from './map/MapApp';
-// import Overview from './overview';
+import Overview from './overview';
 // import Profile from './profile';
-// import TimerList from './timerList';
+ import TimerList from './timerList';
 // import History from './history';
 // import Metrics from './metrics';
 // import Settings from './settings';
@@ -24,12 +24,12 @@ const AppNavigator = DrawerNavigator({
     //  Map: {
     //    screen: MapApp
     //  },
-    //  Camera: {
-    //    screen: CameraApp
-    //  },
-    //  Timers: {
-    //    screen: TimerList
-    //  },
+      Camera: {
+        screen: CameraApp
+      },
+      Timers: {
+        screen: TimerList
+      },
     //  History: {
     //    screen: History
     //  },
@@ -45,7 +45,7 @@ const AppNavigator = DrawerNavigator({
     //  FAQs: {
     //    screen: FAQs
     //  }
- }
+ //}
  // , {
  // drawerWidth: 180,
  // contentComponent: props => (<View style={{flexDirection: 'row'}}>
@@ -81,7 +81,9 @@ const AppNavigator = DrawerNavigator({
  });
 
 export default class App extends Component {
-    render() {
+  render() {
+    this._resetRealmState();
+    console.log('realm', Realm);
         return (
                 <AppNavigator />
         );
@@ -93,17 +95,17 @@ export default class App extends Component {
     //     this.signIn();
     // }
     //
-    // _resetRealmState() { // For beta testing only TODO remove this
-    //     Realm.clearTestState(); // Uncomment to drop/recreate database
-    //     this.realm = new Realm({schema: Schema});
-    //     this.realm.write(() => {
-    //                      this.realm.create('TimerSequence', {timeAccessedAt: new Date() / 1000, count: 0});
-    //                      this.realm.create('TimeLimit', {float: 1, hour: '1', minutes: "00"});
-    //                      this.realm.create('Coordinates', {latitude: 0, longitude: 0});
-    //                      this.realm.create('Ticketed', {list: []});
-    //                      this.realm.create('Expired', {list: []});
-    //                      });
-    // }
+     _resetRealmState() { // For beta testing only TODO remove this
+         Realm.clearTestState(); // Uncomment to drop/recreate database
+         this.realm = new Realm({schema: Schema});
+         this.realm.write(() => {
+                          this.realm.create('TimerSequence', {timeAccessedAt: new Date() / 1000, count: 0});
+                          this.realm.create('TimeLimit', {float: 1, hour: '1', minutes: "00"});
+                          this.realm.create('Coordinates', {latitude: 0, longitude: 0});
+                          this.realm.create('Ticketed', {list: []});
+                          this.realm.create('Expired', {list: []});
+                          });
+     }
     //
     // async signIn() {
     //     let profile = await AsyncStorage.getItem('@Enforce:profileSettings');
