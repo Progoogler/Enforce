8b3030d837a4f48a64849301d582802177d1a2ec
Ray <Ray@rays-air.attlocal.net>
2017-06-28 14:52:53 -0700

    added conditions for new user in reset

--- ios/src/components/overview/ListView-ResetControl.js
+++ ios/src/components/overview/ListView-ResetControl.js
@@ -101,10 +101,11 @@ export default class TimersList extends Component {
       dateCount = JSON.parse(dateCount);
       if (dateCount.length >= 45) {
         let removalDate = dateCount.shift();
-        removeTicketPath(refPath, removalDate); // Delete path from Firebase.
+        refPath && removeTicketPath(refPath, removalDate); // Delete path from Firebase.
       }
     }
-    dateCount.push(registerDate); // Add the previous date to the History list
+    // TODO Consider adding condition to push date only if upload data settings is on
+    registerDate && dateCount.push(registerDate); // Add the previous date to the History list
     dateCount = JSON.stringify(dateCount);
     AsyncStorage.setItem('@Enforce:dateCount', dateCount);
 
