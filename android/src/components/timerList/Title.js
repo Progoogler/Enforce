24a0d7b6394a4b08aa9f47110d10f0d78c96a02f
Andy Li <andyzanli@gmail.com>
2017-07-07 17:02:34 -0700

    decreased margin top

--- android/src/components/timerList/Title.js
+++ android/src/components/timerList/Title.js
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: primaryBlue,
-    marginTop: '-3%',
+    marginTop: '-1%',
   },
   text: {
     fontSize: mediumFontSize,
