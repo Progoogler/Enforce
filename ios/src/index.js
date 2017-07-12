de4a6e04c7303f72b77352c684db381ea8c19c50
Andy <andyzanli@gmail.com>
2017-07-11 23:12:29 -0700

    fixed async operation checking first time access

--- ios/src/index.js
+++ ios/src/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import App from './components/App';
 
 export default class Root extends Component {
-    render() {
-        return (
-                <App/>
-        );
-    }
+  render() {
+    return (
+      <App/>
+    );
+  }
 }
