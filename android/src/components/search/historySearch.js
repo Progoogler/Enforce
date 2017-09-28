adc8d4a3f2b81547f7a830cd64ad6478a85ec0b5
Andy <andyzanli@gmail.com>
2017-09-28 16:45:17 -0700

    Added capability to lookup timer history based on a VIN

--- android/src/components/search/historySearch.js
+++ android/src/components/search/historySearch.js
@@ -1,21 +1,44 @@
-const historySearch = (license: string, realm): object => {
+import Realm from 'realm';
+var realm = new Realm();
+
+const historySearch = (license: string, type?: string): object => {
   if (typeof license !== 'string') return;
   if (license.length > 7) return;
+
   let result = {};
   let ticketed = realm.objects('Ticketed')[0]['list'];
-  for (let i = 0; i < ticketed.length; i++) {
-    if (ticketed[i].license === license) {
-      result['type'] = 'ticketed';
-      result['data'] = ticketed[i];
-      return result;
+
+  if (type === 'vinSearch') {
+    for (let i = 0; i < ticketed.length; i++) {
+      if (ticketed[i].VIN === license) {
+        result['type'] = 'ticketed';
+        result['data'] = ticketed[i];
+        return result;
+      }
     }
-  }
-  let expired = realm.objects('Expired')[0]['list'];
-  for (let i = 0; i < expired.length; i++) {
-    if (expired[i].license === license) {
-      result['type'] = 'expired';
-      result['data'] = expired[i];
-      return result;
+    let expired = realm.objects('Expired')[0]['list'];
+    for (let i = 0; i < expired.length; i++) {
+      if (expired[i].VIN === license) {
+        result['type'] = 'expired';
+        result['data'] = expired[i];
+        return result;
+      }
+    }
+  } else {
+    for (let i = 0; i < ticketed.length; i++) {
+      if (ticketed[i].license === license) {
+        result['type'] = 'ticketed';
+        result['data'] = ticketed[i];
+        return result;
+      }
+    }
+    let expired = realm.objects('Expired')[0]['list'];
+    for (let i = 0; i < expired.length; i++) {
+      if (expired[i].license === license) {
+        result['type'] = 'expired';
+        result['data'] = expired[i];
+        return result;
+      }
     }
   }
   // let timing = this.realm.objects('Timers');
