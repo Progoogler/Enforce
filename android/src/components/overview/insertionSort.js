8b50f4d5eb16f9719aefb80708168da3043d72ca
Andy <andyzanli@gmail.com>
2017-10-08 21:55:52 -0700

    Optimized sort by storing a static value of Date() rather than recomputing its value on each iteration.

--- android/src/components/overview/insertionSort.js
+++ android/src/components/overview/insertionSort.js
@@ -1,28 +1,29 @@
 export default function insertionSortModified(arr: object): array {
-    const aux = [];
+  const aux = [];
+  var now = new Date();
 
-    for (let list in arr) {
-      aux.push(arr[list]);
-    }
+  for (let list in arr) {
+    aux.push(arr[list]);
+  }
 
-    for(let i = 1; i < aux.length; i++) {
-      let temp = aux[i];
-      
-      if (temp.list[0].createdAt === undefined) continue;
-      let tempElapsed = (temp.list[0].timeLength * 60 * 60 * 1000) - (new Date() - temp.list[0].createdAt);
+  for (let i = 1; i < aux.length; i++) {
+    let temp = aux[i];
+    
+    if (temp.list[0].createdAt === undefined) continue;
+    let tempElapsed = (temp.list[0].timeLength * 60 * 60 * 1000) - (now - temp.list[0].createdAt);
 
-      for(let j = i - 1; j >= 0; j--) {
-        if (aux[j].list[0].createdAt === undefined) continue;
-        let earlyElapsed = (aux[j].list[0].timeLength * 60 * 60 * 1000) - (new Date() - aux[j].list[0].createdAt);
+    for (let j = i - 1; j >= 0; j--) {
+      if (aux[j].list[0].createdAt === undefined) continue;
+      let earlyElapsed = (aux[j].list[0].timeLength * 60 * 60 * 1000) - (now - aux[j].list[0].createdAt);
 
-        if (tempElapsed < earlyElapsed) {
-          swap(aux, j+1, j);
-        } else {
-          break
-        }
+      if (tempElapsed < earlyElapsed) {
+        swap(aux, j+1, j);
+      } else {
+        break
       }
     }
-   return aux;
+  }
+  return aux;
 }
 
 function swap(arr, elm1, elm2) {
