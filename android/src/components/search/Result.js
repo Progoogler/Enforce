2c7a680a2f3a93b8db11edb16e41e0348072e8c9
Andy <andyzanli@gmail.com>
2017-10-16 22:25:33 -0700

    Added condition to prevent component rerendering on "searching" state change

--- android/src/components/search/Result.js
+++ android/src/components/search/Result.js
@@ -11,7 +11,6 @@ import PropTypes from 'prop-types';
 import ImageModal from '../history/ImageModal';
 import MapModal from '../history/MapModal';
 import Unfound from './Unfound';
-
 import {
   closeButtonSize,
   imageSize,
@@ -55,52 +54,52 @@ export default class Result extends Component {
             <TouchableOpacity onPress={() => this.maximizeOrMinimizeImage(this.props.data.data.mediaUri)}>
               <Image source={{uri: this.props.data.data.mediaUri}} style={styles.image} />
             </TouchableOpacity>
-              <View style={styles.dataContainer}>
-                { 
-                  this.props.data.data.license && this.props.data.data.VIN ?
-                  <Text><Text style={styles.label}>License:</Text> {this.props.data.data.license + '         '}
-                  <Text style={styles.label}>VIN:</Text> {this.props.data.data.VIN}</Text> 
-                  :
-                  <Text><Text style={styles.label}>License:</Text> {this.props.data.data.license}</Text> 
-                }
-
-                <Text><Text style={styles.label}>Photo taken:</Text> {this._getPrettyTimeFormat(this.props.data.data.createdAt)}</Text>
-                { 
-                  this.props.data.type === 'ticketed' ? 
-                  <Text><Text style={styles.label}>Ticketed:</Text> {this._getPrettyTimeFormat(this.props.data.data.ticketedAt)}</Text> 
-                  : 
-                  <Text><Text style={styles.label}>Expired:</Text> {this._getPrettyTimeFormat(this.props.data.data.ticketedAt)}</Text> 
-                }
-                <Text><Text style={styles.label}>Time limit:</Text> {this._getTimeLimitDesc(this.props.data.data.timeLength)}</Text>
-              </View>
-              {
-                this.props.data.data.latitude || this.props.data.data.description ?  
-                <TouchableOpacity
-                  onPress={() => this._openMapPage()} 
-                  style={styles.mapButton}
-                >
-                  <Image source={require('../../../../shared/images/blue-pin.png')} style={styles.mapIcon}/>
-                </TouchableOpacity>
+            <View style={styles.dataContainer}>
+              { 
+                this.props.data.data.license && this.props.data.data.VIN ?
+                <Text><Text style={styles.label}>License:</Text> {this.props.data.data.license + '         '}
+                <Text style={styles.label}>VIN:</Text> {this.props.data.data.VIN}</Text> 
                 :
-                null
+                <Text><Text style={styles.label}>License:</Text> {this.props.data.data.license}</Text> 
               }
-              <TouchableOpacity
-                onPress={() => this._handleCloseResult()} 
-                opacity={.8}
-                style={styles.closeResultButton}
-              >
-                <Text style={styles.closeResultText}>X</Text>
-              </TouchableOpacity>
+
+              <Text><Text style={styles.label}>Photo taken:</Text> {this._getPrettyTimeFormat(this.props.data.data.createdAt)}</Text>
+              { 
+                this.props.data.type === 'ticketed' ? 
+                <Text><Text style={styles.label}>Ticketed:</Text> {this._getPrettyTimeFormat(this.props.data.data.ticketedAt)}</Text> 
+                : 
+                <Text><Text style={styles.label}>Expired:</Text> {this._getPrettyTimeFormat(this.props.data.data.ticketedAt)}</Text> 
+              }
+              <Text><Text style={styles.label}>Time limit:</Text> {this._getTimeLimitDesc(this.props.data.data.timeLength)}</Text>
             </View>
+            {
+              this.props.data.data.latitude || this.props.data.data.description ?  
+              <TouchableOpacity
+                onPress={() => this._openMapPage()} 
+                style={styles.mapButton}
+              >
+                <Image source={require('../../../../shared/images/blue-pin.png')} style={styles.mapIcon}/>
+              </TouchableOpacity>
+              :
+              null
+            }
+            <TouchableOpacity
+              onPress={() => this._handleCloseResult()} 
+              opacity={.8}
+              style={styles.closeResultButton}
+            >
+              <Text style={styles.closeResultText}>X</Text>
+            </TouchableOpacity>
+          </View>
 
-            :
+          :
 
-            <Unfound 
-              deepSearch={this.props.deepSearch}
-              license={this.props.license} 
-              searching={this.props.searching}
-              type={this.props.data}
-            />
+          <Unfound 
+            deepSearch={this.props.deepSearch}
+            license={this.props.license} 
+            searching={this.props.searching}
+            type={this.props.data}
+          />
         }
 
         { 
@@ -120,15 +119,16 @@ export default class Result extends Component {
     );
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.props.searching !== nextProps.searching) return true;
+  shouldComponentUpdate(nextProps, nextState) { 
     if (this.state.modalVisible !== nextState.modalVisible) return true;
     if (this.state.showMaximizedImage !== nextState.showMaximizedImage) return true;
     if (this.state.uri !== nextState.uri) return true;
+    if (nextProps.searching === false) return false;
+    if (this.props.searching !== nextProps.searching) return true;
     return false;
   }
 
-  _getTimeLimitDesc = (timeLimit: number): string => {
+  _getTimeLimitDesc(timeLimit: number): string {
     if (timeLimit < 1) {
       return `${timeLimit * 60} minutes`;
     } else {
@@ -136,7 +136,7 @@ export default class Result extends Component {
     }
   }
 
-  _getPrettyTimeFormat = (createdAt: number): string => {
+  _getPrettyTimeFormat(createdAt: number): string {
     let date = new Date(createdAt);
     let hour = date.getHours();
     let minutes = date.getMinutes() + '';
@@ -181,36 +181,6 @@ Result.propTypes = {
 };
 
 const styles = StyleSheet.create({
-  outerContainer: {
-    alignSelf: 'stretch',
-    backgroundColor: primaryBlue,
-    height: 120,
-    padding: '4%',
-  },
-  container: {
-    backgroundColor: 'white',
-    flex: 1,
-    flexDirection: 'row',
-  },
-  image: {
-    flex: .3,
-    height: imageSize,
-    marginRight: '4%',
-    width: imageSize,
-  },
-  dataContainer: {
-    flex: .7,
-    padding: '1.5%',
-  },
-  label: {
-    fontSize: smallFontSize,
-    fontWeight: 'bold',
-  },
-  mapButton: {
-    bottom: 0,
-    position: 'absolute',
-    right: 0,
-  },
   closeResultButton: {
     backgroundColor: primaryBlue,
     height: closeButtonSize,
@@ -221,9 +191,38 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  dataContainer: {
+    flex: .7,
+    padding: '1.5%',
+  },
+  image: {
+    flex: .3,
+    height: imageSize,
+    marginRight: '4%',
+    width: imageSize,
+  },
+  label: {
+    fontSize: smallFontSize,
+    fontWeight: 'bold',
+  },
+  mapButton: {
+    bottom: 0,
+    position: 'absolute',
+    right: 0,
+  },
   mapIcon: {
     height: pinHeight,
     width: pinWidth,
   },
+  outerContainer: {
+    alignSelf: 'stretch',
+    backgroundColor: primaryBlue,
+    height: 120,
+    padding: '4%',
+  },
 });
- 
